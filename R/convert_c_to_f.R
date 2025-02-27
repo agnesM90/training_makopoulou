d8f9548ba949422bf8384145d7b90994782c1476
agnesM90 <eirini.makopoulou@oulu.fi>
2025-02-27 14:11:41 -0800

    Commit

---
+++ R/convert_c_to_f.R
@@ -0,0 +1,11 @@
+#'My temperature conversion
+#'
+#'
+
+
+
+
+convert_c_to_f <- function(celsius) {
+  fahr <- celsius * 9/5 + 32
+  return(fahr)
+}
---
+++ tests/testthat/test-convert_f_to_c.R
@@ -0,0 +1,3 @@
+test_that("fahr to celsius works", {
+  expect_equal(convert_f_To_c(32), 0)
+})
